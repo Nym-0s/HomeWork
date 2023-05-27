01947b4b07954dd1019503e54fb896c363ecfe6b
Lina <lotosofhellforyou@gmail.com>
2023-05-27 13:33:56 +0300

    Added homework

---
+++ HomeWorkWeb/GameStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GameStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Concrete
+{
+  public class EFDbContext : DbContext
+  {
+    //public EFDbContext(string connectionString) : base(nameOrConnectionString: connectionString)
+    public EFDbContext(string nameOrConnectionString) : base(nameOrConnectionString) { }
+
+    public DbSet<Game> Games { get; set; }
+
+  }
+}
---
+++ HomeWorkWeb/GameStore.WebUI/Global.asax.cs
@@ -0,0 +1,18 @@
+using GameStore.Domain.Entities;
+using GameStore.WebUI.Infrastructure.Binders;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace GameStore.WebUI
+{
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    protected void Application_Start()
+    {
+      AreaRegistration.RegisterAllAreas();
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
+
+    }
+  }
+}
